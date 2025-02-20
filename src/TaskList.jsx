ee5a178989f6ab95517b3f3760d992237d369130
Anna <31605341+Asya0@users.noreply.github.com>
2025-02-20 14:38:24 +0600

    feat: update style for tailwind css, check empty string

--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -1,29 +1,46 @@
-import { useState, React } from "react";
+import { useState, useEffect, React } from "react";
 import Task from "./Task";
 
 import { IoIosAdd } from "react-icons/io";
 
 const TaskList = ({ tasks, onClick, onRemove, onAdd }) => {
   const [isValue, setIsValue] = useState("");
+  const [taskList, setTaskList] = useState(tasks);
+
+  //загружаем задачи из localStorage при каждом измненении
+  useEffect(() => {
+    const savedTasks = localStorage.getItem("tasks");
+    if (savedTasks) setTaskList(JSON.parse(savedTasks));
+  }, []);
+
+  //сохранение в localStorage при каждом изменении
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(taskList));
+  }, []);
+
+  const handleAddTask = () => {
+    if (isValue === "" || isValue.trim() === "") {
+      alert("введите название задачи");
+      setIsValue("");
+      return;
+    }
+    onAdd(isValue);
+    setIsValue("");
+  };
 
   return (
     <>
-      <div className="">
+      <div className="flex items-center justify-between mb-2.5 p-2.5 rounded-sm">
         <input
           type="text"
-          value={tasks.title}
+          value={isValue}
           placeholder="название задачи"
           onChange={(e) => {
             setIsValue(e.target.value);
             console.log(isValue);
           }}
         />
-        {/* value={isValue} */}
-        <IoIosAdd
-          size={40}
-          className="add-task"
-          onClick={() => onAdd(isValue)}
-        />
+        <IoIosAdd size={40} className="" onClick={handleAddTask} />
       </div>
 
       {tasks.map((task) => (
