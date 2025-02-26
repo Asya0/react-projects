0b8e8d96b6486bed1afbb7c9036c1f1f7c643735
Anna <31605341+Asya0@users.noreply.github.com>
2025-02-26 19:33:53 +0600

    feat: saving  tasks in localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import { useState, React } from "react";
+import { useState, useEffect, React } from "react";
 import TaskList from "./TaskList";
 import "./App.css";
 
 const App = () => {
+  const [taskList, setTaskList] = useState(() => {
+    const savedTasks = localStorage.getItem("tasks");
+    const parsedTasks = savedTasks ? JSON.parse(savedTasks) : [];
+    console.log("Loaded tasks from localStorage:", parsedTasks);
+    return parsedTasks;
+  });
+  const [isValue, setIsValue] = useState("");
+
   const [tasks, setTasks] = useState([
     { id: 1, title: " work ", isCompleted: true },
     { id: 2, title: " покурить ", isCompleted: false },
@@ -11,42 +19,68 @@ const App = () => {
     { id: 5, title: " учеба ", isCompleted: false },
   ]);
 
-  //TODO находить нужнуую задачу и менять ее isChecked, по id
+  // Сохранение задач в localStorage при изменении taskList
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(taskList));
+    console.log("taskList", taskList);
+    // сохраняет, но не отображает на экране все задачи
+  }, [taskList]);
+
+  //TODO находить нужную задачу и менять ее isChecked, по id
   const checkTask = (id) => {
-    setTasks((prevTasks) =>
+    setTaskList((prevTasks) =>
       prevTasks.map((t) =>
         t.id === id ? { ...t, isCompleted: !t.isCompleted } : t
       )
     );
   };
   const removeTask = (id) => {
-    setTasks((prevTasks) => {
-      const filteredTasks = prevTasks.filter((t) => t.id !== id);
-      return filteredTasks;
-    });
+    setTaskList((prevTasks) => prevTasks.filter((t) => t.id !== id));
   };
 
   const addTask = (title) => {
-    setTasks((prevTasks) => {
+    setTaskList((prevTasks) => {
+      if (isValue === "" || isValue.trim() === "") {
+        alert("введите название задачи");
+        setIsValue("");
+        return;
+      }
       const newTask = { id: prevTasks.length + 1, title, isCompleted: false };
       return [...prevTasks, newTask];
     });
   };
+  const handleAddTask = () => {
+    if (isValue == "" || isValue.trim() === "") {
+      alert("введите название задачи");
+      setIsValue("");
+      return;
+    }
+    const newTask = {
+      id: tasks.length + 1,
+      title: isValue,
+      isCompleted: false,
+    };
+    setTaskList((prev) => [...prev, newTask]);
+    setIsValue("");
+  };
 
   return (
     <>
-      <div className="container">
-        <div className="header">
+      <div className="max-w-7xl w-full flex flex-col">
+        <div className="flex items-center justify-center">
           <h1 className="text-3xl font-bold underline hover:text-sky-300">
             To do:
           </h1>
         </div>
-        <div className="main">
+        <div className="flex flex-col items-start gap-y-4">
           <TaskList
-            tasks={tasks}
-            onClick={checkTask}
+            tasks={taskList}
+            onCheck={checkTask}
             onRemove={removeTask}
             onAdd={addTask}
+            handleAddTask={handleAddTask}
+            isValue={isValue}
+            setIsValue={setIsValue}
           />
         </div>
       </div>
