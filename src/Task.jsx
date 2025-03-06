1b7b96f9865d6e6b7c774fbfb46722cacab71da5
Anna <31605341+Asya0@users.noreply.github.com>
2025-03-06 18:06:40 +0600

    fix: added onCheck

--- src/Task.jsx
+++ src/Task.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { CiTrash } from "react-icons/ci";
 
-const Task = ({ id, title, isCompleted, onClick, onRemove }) => {
+const Task = ({ id, title, isCompleted, onClick, onRemove, onCheck }) => {
   return (
     <>
       <div className="w-full rounded-lg border border-gray-200 flex items-center justify-between p-4">
@@ -10,9 +10,16 @@ const Task = ({ id, title, isCompleted, onClick, onRemove }) => {
           className="mr-2 w-6 h-6 rounded-lg"
           type="checkbox"
           checked={isCompleted}
-          onChange={() => onClick(id)}
+          onChange={() => {
+            onCheck(id);
+          }}
         />
         <span className="flex-grow">{title}</span>
+        {/* <button
+          onClick={() => onEdit(id, prompt("Редактировать задачу", title))}
+        >
+          Редактировать
+        </button> */}
         <CiTrash
           className="cursor-pointer"
           size={30}
